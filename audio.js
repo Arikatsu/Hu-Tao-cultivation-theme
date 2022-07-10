a51e82f380c9737ff2348e009a222e5c659a5255
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-07-10 12:41:20 +0530

    stuff

---
+++ audio.js
@@ -0,0 +1,29 @@
+const huAudio = document.createElement('img');
+huAudio.id = "audioButton";
+huAudio.src = "https://cdn.discordapp.com/attachments/981291804202778664/995225977489719366/unknown.png"
+huAudio.style.width = "100px";
+huAudio.style.position = 'absolute';
+huAudio.style.top = '90%';
+huAudio.style.left = '53%';
+huAudio.style.transform = 'translate(-50%, -50%)';
+huAudio.style.cursor = 'pointer';
+
+document.body.appendChild(huAudio);
+
+const audio = document.createElement('audio');
+audio.id = "bgAudio";
+audio.src = "https://cdn.discordapp.com/attachments/981291804202778664/995218109789114459/tao.mp3";
+
+var isPlaying = true;
+audio.play();
+
+huAudio.addEventListener('click', () => {
+    if (isPlaying) {
+        audio.pause();
+        isPlaying = false;
+    } else {
+        audio.play();
+        audio.loop = true;
+        isPlaying = true;
+    }
+})
---
+++ news.js
@@ -0,0 +1,26 @@
+const news = document.querySelector('.NewsSection');
+news.style.display = 'block';
+
+const newsButton = document.createElement('img');
+newsButton.id = "audioButton";
+newsButton.src = "https://cdn.discordapp.com/attachments/991102367636402207/995585976107798558/unknown.png"
+newsButton.style.width = "100px";
+newsButton.style.position = 'absolute';
+newsButton.style.top = '25%';
+newsButton.style.left = '5%';
+newsButton.style.transform = 'translate(-50%, -50%)';
+newsButton.style.cursor = 'pointer';
+
+document.body.appendChild(newsButton);
+
+var newsDisplay = true;
+
+newsButton.addEventListener('click', () => {
+    if (newsDisplay) {
+        news.style.display = 'none';
+        newsDisplay = false;
+    } else {
+        news.style.display = 'block';
+        newsDisplay = true;
+    }
+})
